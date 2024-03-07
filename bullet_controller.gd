2747a3c6ad07d66dff2890dfa3b8e3ab535764f6
EduardoMeloC <33487458+EduardoMeloC@users.noreply.github.com>
2024-03-06 21:19:16 -0300

    Optimized bullet with pooling

--- bullet_controller.gd
+++ bullet_controller.gd
@@ -1,10 +1,6 @@
 @tool
 extends Node2D
 
-const LinearBullet = preload("res://linear_bullet.tscn")
-
-@onready var _player = get_tree().get_nodes_in_group("Player")[0]
-
 @export_range(0, 360) var _shoot_radius : int = 60:
 	set(new_value):
 		_shoot_radius = new_value
@@ -20,7 +16,8 @@ const LinearBullet = preload("res://linear_bullet.tscn")
 		_debug_radius = new_value
 		queue_redraw()
 
-var player_direction := Vector2.ONE
+@onready var _player = get_tree().get_nodes_in_group("Player")[0]
+@onready var _pool = $BulletPool
 		
 func _draw():
 	if Engine.is_editor_hint():
@@ -31,18 +28,24 @@ func _draw():
 			draw_circle(dir*_debug_radius, 3, Color.WHITE)
 
 func spawn_bullets_radial():
-	player_direction = (_player.position - self.position).normalized() 
-	queue_redraw()
+	var player_direction = (_player.position - self.position).normalized() 
 	var angle_step = deg_to_rad(_shoot_radius)/_bullet_count
 	for i in range(-_bullet_count/2, _bullet_count/2+1):
 		var dir = player_direction.rotated(i*angle_step).normalized()
-		var bullet = LinearBullet.instantiate()
-		bullet.direction = dir
-		bullet.look_at(dir)
-		self.add_child(bullet)
-
+		var bullet : Bullet = _pool.get_object().initialize(self.position, dir)
+		
+func spawn_bullets_faster():	
+	var player_direction = (_player.position - self.position).normalized() 
+	var angle_step = deg_to_rad(_shoot_radius)/_bullet_count
+	for i in range(-_bullet_count/2, _bullet_count/2+1):
+		var dir = player_direction.rotated(i*angle_step).normalized()
+		var bullet : Bullet = _pool.get_object().initialize(self.position, dir)
+		bullet.set_direction_over_time(func(direction : Vector2, delta : float):
+			return direction * 1.01)
 
 func _input(event):
 	var just_pressed = event.is_pressed() and not event.is_echo()
 	if Input.is_key_pressed(KEY_SPACE) and just_pressed:
 		spawn_bullets_radial()
+	if Input.is_key_pressed(KEY_F1) and just_pressed:
+		spawn_bullets_faster()
---
+++ bullet_pool.gd
@@ -0,0 +1,23 @@
+extends Node
+
+class_name BulletPool
+var _bullet_scene = preload("res://resources/Bullet.tscn")
+
+var _pool_size = 4096
+var _pool = []
+var _index = 0
+
+func _ready():
+	preload_objects()
+	
+func preload_objects():
+	for i in range(_pool_size):
+		var bullet : Bullet = _bullet_scene.instantiate()
+		bullet.deactivate()
+		_pool.append(bullet)
+		self.add_child(bullet)
+
+func get_object():
+	var obj = _pool[_index]
+	_index = (_index + 1) % _pool_size
+	return obj
